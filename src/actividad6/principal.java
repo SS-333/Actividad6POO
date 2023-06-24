553bebfd59fac79bf260bef3a4bce8ca68a80e08
Sergio Romero <seromeros@unal.edu.co>
2023-06-24 13:21:17 -0500

    Actividad 6

---
+++ src/actividad6/principal.java
@@ -0,0 +1,10 @@
+package actividad6;
+
+public class principal {
+
+    public static void main(String[] args) {
+        Interfaz interfaz = new Interfaz();
+        interfaz.setVisible(true);
+    }
+    
+}
